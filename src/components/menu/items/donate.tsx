6baf3d5285f3881721ab9cf94ab422668b3b41ea
liuyi <1647495684@qq.com>
2025-03-04 10:08:24 +0800

    remove auth

--- src/components/menu/items/donate.tsx
+++ src/components/menu/items/donate.tsx
@@ -4,10 +4,10 @@ import { Item } from '../item';
 
 export function Donate() {
   return (
-    <Item
-      href="https://buymeacoffee.com/remvze"
-      icon={<SiBuymeacoffee />}
-      label="Buy Me a Coffee"
-    />
+    // <Item
+    //   href="https://buymeacoffee.com/remvze"
+    //   icon={<SiBuymeacoffee />}
+    //   label="Buy Me a Coffee"
+    // />
   );
 }
